acdd4de249cc0c0fc9c656f84ef4d19077f5e2a5
ramiro <ramiroalvaro@hotmail.com>
2022-03-07 18:44:15 -0300

    first commit

---
+++ src/category/tests/unit/domain/test_unit_entities.py
@@ -0,0 +1,23 @@
+import unittest
+from dataclasses import is_dataclass
+from datetime import datetime
+
+from src.category.domain.entities import Category
+
+
+class TestCategoryUnit(unittest.TestCase):
+
+    def test_if_is_a_dataclass(self):
+        self.assertTrue(is_dataclass(Category))
+
+    def test_constructor(self):
+        category = Category(
+            name='Movie',
+            description='some description',
+            is_active=True,
+            created_at=datetime.now()
+        )
+        self.assertEqual(category.name, 'Movie')
+        self.assertEqual(category.description, 'some description')
+        self.assertEqual(category.is_active, True)
+        self.assertIsInstance(category.created_at, datetime)
